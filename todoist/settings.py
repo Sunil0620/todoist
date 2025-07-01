1c503a42ad936b01b1131ac99c4de840c2607983
Sunil0620 <sunilsaini5652@gmail.com>
2025-07-01 18:36:57 +0530

    updated settings.py

--- todoist/settings.py
+++ todoist/settings.py
@@ -78,19 +78,15 @@ WSGI_APPLICATION = 'todoist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ and os.environ['DATABASE_URL']:
     DATABASES = {
         'default': dj_database_url.parse(os.environ['DATABASE_URL'])
     }
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME', 'TasksDB'),
-            'USER': os.environ.get('DB_USER', 'postgres'),
-            'PASSWORD': os.environ.get('DB_PASSWORD', '123as'),
-            'HOST': os.environ.get('DB_HOST', 'localhost'),
-            'PORT': os.environ.get('DB_PORT', '5432'),
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
